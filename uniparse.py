73398153a45b23d89e9e40b2ce26b7c91e934d21
Guy Smith <guylsmith1972@gmail.com>
2024-05-03 12:23:50 -0700

    uniparse.py experimentation

--- uniparse.py
+++ uniparse.py
@@ -1,25 +1,5 @@
 from tree_sitter_languages import get_language, get_parser
 
-python_code = """
-class MyClass:
-    def my_method(self, value):
-        return value == 42
-
-def foo():
-    pass
-"""
-
-c_code = """
-class Foo {
-public:
-    void bar();
-}
-
-int main(int argc, char**argv) {
-    return 0
-}
-"""
-
 
 python_specification = {
     "class_definition": ["class", "identifier"],
@@ -45,25 +25,58 @@ def parse(code, language):
     return tree.root_node
 
 # Function to recursively print only class and function names
-def print_tree(node, language, level=0, source_code=None):
+def print_tree(node, language, context, source_code=None):
     specification = languages[language]
     node_type = specification[node.type] if node.type in specification else None
+    
+    extended_context = False
+    
     if node_type is not None and (node_type[0] == 'class' or node_type[0] == 'function'):
         subtype = node_type[1]
         name_node = next((child for child in node.children if child.type == subtype), None)
         if name_node:
-            name = name_node.text
-            prefix = '  ' * level  # Add indentation based on the tree level
-            print(f"{prefix}Name: {name}")
+            name = name_node.text.decode("utf-8")
+            context.append(name)
+            extended_context = True
+            print(f"Name: {'.'.join(context)}")
 
     # Recursively process children
     for child in node.children:
-        print_tree(child, language, level+1, source_code)
+        print_tree(child, language, context, source_code)
 
-print('-' * 79)
-root_node = parse(python_code, 'python')
-print_tree(root_node, 'python', source_code=python_code)
+    if extended_context:
+        context.pop()
 
-print('-' * 79)
-root_node = parse(c_code, 'cpp')
-print_tree(root_node, 'cpp', source_code=c_code)
+def main():
+    python_code = """
+    class MyClass:
+        def my_method(self, value):
+            return value == 42
+
+    def foo():
+        pass
+    """
+
+    c_code = """
+    class Foo {
+    public:
+        void bar();
+    }
+
+    int main(int argc, char**argv) {
+        return 0
+    }
+    """
+
+
+    print('-' * 79)
+    root_node = parse(python_code, 'python')
+    print_tree(root_node, 'python', [], source_code=python_code)
+
+    print('-' * 79)
+    root_node = parse(c_code, 'cpp')
+    print_tree(root_node, 'cpp', [], source_code=c_code)
+
+if __name__ == '__main__':
+    main()
+    
