a71319222b3e6e4ce6cc517796ac76798fba0036
Guy Smith <guylsmith1972@gmail.com>
2024-05-03 13:05:45 -0700

    Further fiddling with tree-sitter in uniparse.py

--- uniparse.py
+++ uniparse.py
@@ -2,13 +2,13 @@ from tree_sitter_languages import get_language, get_parser
 
 
 python_specification = {
-    "class_definition": ["class", "identifier"],
-    "function_definition": ["function", "identifier"]
+    "class_definition": ["class", ["identifier"]],
+    "function_definition": ["function", ["identifier"]]
 }
 
 cpp_specification = {
-    "class_specifier": ["class", "identifier"],
-    "function_declarator": ["function", "identifier"]
+    "class_specifier": ["class", ["type_identifier"]],
+    "function_definition": ["function", [lambda nt: nt=="function_declarator" or nt=="parenthesized_declarator", lambda nt: nt == "field_identifier" or nt == "identifier"]]
 }
 
 languages = {
@@ -24,8 +24,23 @@ def parse(code, language):
     tree = parser.parse(bytes(code, "utf8"))
     return tree.root_node
 
-# Function to recursively print only class and function names
-def print_tree(node, language, context, source_code=None):
+
+def print_tree(node, language, context, level=0, source_code=None):
+
+    print(f'node.type: {"  " * level}{node.type} -- {node.text}')
+
+    # Recursively process children
+    for child in node.children:
+        print_tree(child, language, context, level+1, source_code)
+
+
+def print_functions(node, language, context, source_code=None):
+    def get_element(node, sequence):
+        comparator = sequence[0] if callable(sequence[0]) else lambda nt: nt == sequence[0]
+        next_node = next((child for child in node.children if comparator(child.type)), None)
+        return next_node if len(sequence) == 1 else get_element(next_node, sequence[1:]) if next_node else None
+
+
     specification = languages[language]
     node_type = specification[node.type] if node.type in specification else None
     
@@ -33,16 +48,19 @@ def print_tree(node, language, context, source_code=None):
     
     if node_type is not None and (node_type[0] == 'class' or node_type[0] == 'function'):
         subtype = node_type[1]
-        name_node = next((child for child in node.children if child.type == subtype), None)
+        name_node = get_element(node, subtype)
         if name_node:
             name = name_node.text.decode("utf-8")
             context.append(name)
             extended_context = True
             print(f"Name: {'.'.join(context)}")
+            if node_type[0] == 'function':
+               code_body = node.text.decode("utf-8")
+               print(code_body)
 
     # Recursively process children
     for child in node.children:
-        print_tree(child, language, context, source_code)
+        print_functions(child, language, context, source_code)
 
     if extended_context:
         context.pop()
@@ -60,22 +78,30 @@ def main():
     c_code = """
     class Foo {
     public:
-        void bar();
-    }
-
+        void (do_nothing) {}
+    };
+    
     int main(int argc, char**argv) {
         return 0
     }
     """
 
 
-    print('-' * 79)
+    print('=' * 79)
     root_node = parse(python_code, 'python')
     print_tree(root_node, 'python', [], source_code=python_code)
 
     print('-' * 79)
+    root_node = parse(python_code, 'python')
+    print_functions(root_node, 'python', [], source_code=python_code)
+
+    print('=' * 79)
     root_node = parse(c_code, 'cpp')
     print_tree(root_node, 'cpp', [], source_code=c_code)
+
+    print('-' * 79)
+    root_node = parse(c_code, 'cpp')
+    print_functions(root_node, 'cpp', [], source_code=c_code)
 
 if __name__ == '__main__':
     main()
