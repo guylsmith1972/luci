65e5031382714c24f4aa16579173dd9a0e8eee3b
Guy Smith <guylsmith1972@gmail.com>
2024-05-03 12:13:34 -0700

    Improved uniparse.py

--- uniparse.py
+++ uniparse.py
@@ -1,22 +1,69 @@
 from tree_sitter_languages import get_language, get_parser
 
-
-parser = get_parser('python')
-parser.set_language(get_language('python'))
-
-code = """
+python_code = """
 class MyClass:
-    def my_method(self):
-        pass
+    def my_method(self, value):
+        return value == 42
+
+def foo():
+    pass
 """
 
-tree = parser.parse(bytes(code, "utf8"))
-root_node = tree.root_node
+c_code = """
+class Foo {
+public:
+    void bar();
+}
 
-# Function to recursively print the tree
-def print_tree(node, level=0):
-    print('  ' * level + node.type, node.start_point, node.end_point)
+int main(int argc, char**argv) {
+    return 0
+}
+"""
+
+
+python_specification = {
+    "class_definition": ["class", "identifier"],
+    "function_definition": ["function", "identifier"]
+}
+
+cpp_specification = {
+    "class_specifier": ["class", "identifier"],
+    "function_declarator": ["function", "identifier"]
+}
+
+languages = {
+    "c": cpp_specification,
+    "cpp": cpp_specification,
+    "python": python_specification
+}
+
+def parse(code, language):
+    parser = get_parser(language)
+    parser.set_language(get_language(language))
+
+    tree = parser.parse(bytes(code, "utf8"))
+    return tree.root_node
+
+# Function to recursively print only class and function names
+def print_tree(node, language, level=0, source_code=None):
+    specification = languages[language]
+    node_type = specification[node.type] if node.type in specification else None
+    if node_type is not None and (node_type[0] == 'class' or node_type[0] == 'function'):
+        subtype = node_type[1]
+        name_node = next((child for child in node.children if child.type == subtype), None)
+        if name_node:
+            name = name_node.text
+            prefix = '  ' * level  # Add indentation based on the tree level
+            print(f"{prefix}Name: {name}")
+
+    # Recursively process children
     for child in node.children:
-        print_tree(child, level+1)
+        print_tree(child, language, level+1, source_code)
 
-print_tree(root_node)
+print('-' * 79)
+root_node = parse(python_code, 'python')
+print_tree(root_node, 'python', source_code=python_code)
+
+print('-' * 79)
+root_node = parse(c_code, 'cpp')
+print_tree(root_node, 'cpp', source_code=c_code)
